79593a20a374ce0e6604385cd5592ef7fb7adb25
antselley@gmail.com <antselley@gmail.com>
2016-10-17 12:16:59 +0200

    Print shopping list complete

--- printShoppingList.js
+++ printShoppingList.js
@@ -1,1 +1,14 @@
-Just testing github
+
+var shoppingList = [
+    { itemName : 'Bread', price : 11.00 },
+    { itemName : 'Milk', price : 7.00 },
+    { itemName : 'Bread', price : 19.50 }
+];
+
+var printShoppingList = function() {
+  for (var item in shoppingList) {
+    console.log(shoppingList[item].itemName + " @ " + shoppingList[item].price);
+  }
+}
+
+printShoppingList();
