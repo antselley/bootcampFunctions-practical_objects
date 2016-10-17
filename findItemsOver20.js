8f501cee9ba12c47107da9a63b7642a26bbcc0cd
antselley@gmail.com <antselley@gmail.com>
2016-10-17 12:32:29 +0200

    findItemsOver20 sorted

---
+++ findItemsOver20.js
@@ -0,0 +1,28 @@
+
+var findItemsOver20 = function (listOfObjects) {
+  var listOfOverR20 = [];
+  for (object in listOfObjects) {
+    if (listOfObjects[object].price >20) {
+      listOfOverR20.push(listOfObjects[object].itemName);
+    }
+  }
+  return listOfOverR20;
+}
+
+
+var shoppingList1 = [
+    { itemName : 'Bread', price : 11.00 },
+    { itemName : 'Milk', price : 7.00 },
+    { itemName : 'Cheese', price : 19.50 }
+];
+
+var shoppingList2 = [
+    { itemName : 'Apples', price : 7.50 },
+    { itemName : 'Bread', price : 11.00 },
+    { itemName : 'Milk', price : 7.00 },
+    { itemName : 'Cheese', price : 23.00 },
+    { itemName : 'Bread', price : 9.50 },
+    { itemName : 'Digital Watch' , price: 250 }
+];
+
+console.log(findItemsOver20(shoppingList2));
